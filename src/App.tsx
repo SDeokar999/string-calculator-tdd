4096cdc33373720ed17f2013ff862c56582ad7d5
SDeokar999 <sumitdeokar2012@gmail.com>
2024-10-08 11:16:15 +0530

    first test case for String calculator text

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>String calculator</h1>
+      <h1 className="text-base md:text-lg lg:text-xl mx-10 mt-10">
+        String Calculator
+      </h1>
     </div>
   );
 }
