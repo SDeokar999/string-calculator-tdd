a1a47aae06b96b015be67e9a435e1652a8a78db1
SDeokar999 <sumitdeokar2012@gmail.com>
2024-10-08 11:48:41 +0530

    Added the test cases for all scenario

--- src/StringCalculatorUI.test.tsx
+++ src/StringCalculatorUI.test.tsx
@@ -13,3 +13,69 @@ test("renders calculator button", () => {
   const buttonElement = screen.getByTestId("calculatorButton");
   expect(buttonElement).toBeInTheDocument();
 });
+
+test("gives the correct outpt for 1,2,3 ", () => {
+  render(<StringCalculatorUI />);
+  const inputElements = screen.getByTestId("stringInputBox");
+  fireEvent.change(inputElements, { target: { value: "1,2,3" } });
+  const buttonclick = screen.getByTestId("calculatorButton");
+  fireEvent.click(buttonclick);
+  expect(screen.getByText("Result: 6")).toBeInTheDocument();
+});
+
+test("checking for negative numbers ", () => {
+  render(<StringCalculatorUI />);
+  const inputElementNegative = screen.getByTestId("stringInputBox");
+  fireEvent.change(inputElementNegative, { target: { value: "-1,-2" } });
+  const buttonclick = screen.getByTestId("calculatorButton");
+  fireEvent.click(buttonclick);
+  expect(
+    screen.getByText("Negative numbers not allowed: -1, -2")
+  ).toBeInTheDocument();
+});
+
+test("checking for addition of numbers from delimiter ", () => {
+  render(<StringCalculatorUI />);
+  const inputElementsDelimiter = screen.getByTestId("stringInputBox");
+  fireEvent.change(inputElementsDelimiter, { target: { value: "//;\n2,4,3" } });
+  const buttonclick = screen.getByTestId("calculatorButton");
+  fireEvent.click(buttonclick);
+  expect(screen.getByText("Result: 9")).toBeInTheDocument();
+});
+
+test("checking for negative numbers from delimiter scenario 1", () => {
+  render(<StringCalculatorUI />);
+  const inputElementsScenario = screen.getByTestId("stringInputBox");
+  fireEvent.change(inputElementsScenario, { target: { value: "//;\n2;-9;3" } });
+  const buttonclick = screen.getByTestId("calculatorButton");
+  fireEvent.click(buttonclick);
+  expect(
+    screen.getByText("Negative numbers not allowed: -9")
+  ).toBeInTheDocument();
+});
+
+test("checking for negative numbers from delimiter scenario 2 ", () => {
+  render(<StringCalculatorUI />);
+  const inputElementsScenarios = screen.getByTestId("stringInputBox");
+  fireEvent.change(inputElementsScenarios, {
+    target: { value: "//;\n2,-9,1,3" },
+  });
+  const buttonclick = screen.getByTestId("calculatorButton");
+  fireEvent.click(buttonclick);
+  expect(
+    screen.getByText("Negative numbers not allowed: -9")
+  ).toBeInTheDocument();
+});
+
+test("checking for negative numbers from delimiter scenario 2 ", () => {
+    render(<StringCalculatorUI />);
+    const inputElementsEmpty = screen.getByTestId("stringInputBox");
+    fireEvent.change(inputElementsEmpty, {
+      target: { value: "" },
+    });
+    const buttonclick = screen.getByTestId("calculatorButton");
+    fireEvent.click(buttonclick);
+    expect(
+      screen.getByText("Result: 0")
+    ).toBeInTheDocument();
+  });
